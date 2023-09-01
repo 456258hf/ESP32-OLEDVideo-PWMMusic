3b04d14695153579d4307afc9f26bbd405ae6f0d
456258hf <456258hf@gmail.com>
2023-09-02 04:31:20 +0800

    much more pythonic

--- mpycode/main.py
+++ mpycode/main.py
@@ -1,8 +1,14 @@
 from ssd1306 import SSD1306_I2C
 from machine import Pin, Timer
-from musicscore import MUSIC
 import machine
 import os
+
+try:
+    from musicscore import MUSIC
+except ImportError:
+    HasMusic = False
+else:
+    HasMusic = True
 
 machine.freq(240000000)
 
@@ -21,30 +27,30 @@ SPEEDMS = (60000/BPM)/16-1.5  # 计算16音符时值，-1.5以避免音符间隔
 
 
 def time0_irq(time0):
-    global MusicCount
-    MusicFreq = MUSIC[MusicCount][0]  # 选择音符对应的频率
-    beep = machine.PWM(Pin(BEEP_PIN))  # 每次初始化PWM，防止无法去初始化
-    if (MusicFreq):
-        beep.freq(int(440*2**((MusicFreq-10)/12)))  # 十二平均律计算频率，以A4=440Hz为基准
-    else:
-        beep.deinit()  # 休止符
-    time0.init(period=int(  # 设定下一个音的延时
-        (SPEEDMS * MUSIC[MusicCount][1])), mode=Timer.ONE_SHOT, callback=time0_irq)
-    MusicCount += 1  # 下一个音
-    if (MUSIC[MusicCount][0] == 255):  # 如果下一个音是终止标志位，关闭PWM与定时器
+    try:
+        Note = next(Music)
+    except StopIteration:
         time0.deinit()
-        beep.deinit
+        beep.deinit()
+        return
+    if Note[0]:
+        # 十二平均律计算频率，以A4=440Hz为基准
+        beep.duty(512)
+        beep.freq(int(440*2**((Note[0]-10)/12)))
+    else:
+        beep.duty(0)
+    time0.init(period=int(  # 设定下一个音的延时
+        (SPEEDMS * Note[1])), mode=Timer.ONE_SHOT, callback=time0_irq)
 
 
 def time1_irq(time1):
-    global FrameCount, f, p
-    f.seek(1024*FrameCount)  # 从当前帧指针位读取1kb图像数据
-    data = f.read(1024)
-    oled.buffer = data  # 直接写进帧缓存
+    global FrameCount
+    oled.buffer = f.read(1024)  # 读取1kb图像数据，直接写进帧缓存
     oled.show()
-    FrameCount += 1  # 下一帧
-    if (FrameCount == p):  # 如果所有帧播放完毕
+    FrameCount += 1  # 帧计数+1
+    if (FrameCount == FrameNum):  # 如果所有帧播放完毕
         time1.deinit()
+        f.close()
 
 
 if __name__ == "__main__":
@@ -55,18 +61,27 @@ if __name__ == "__main__":
     for file in files:
         if (file.startswith('video')):  # 匹配文件名
             fname = file
-    try:
-        p = int(fname.split('-')[1][:5])  # 读取文件名中帧总数
-    except:
-        p = 1  # 未定义帧总数则仅播放一帧
-    try:
-        fps = int(fname.split('-')[2][:2])  # 读取文件名中fps
-    except:
-        fps = 30  # 未定义fps则默认为30
 
     f = open(f'/sd/{fname}', 'rb')  # 打开文件
 
-    time0 = Timer(0)  # 创建time0定时器对象：控制蜂鸣器发声
-    time0.init(period=1, mode=Timer.ONE_SHOT, callback=time0_irq)
+    # 从文件名中获取视频信息
+    fname = fname.split('-')
+    try:
+        FrameNum = int(fname[1][:5])  # 读取文件名中帧总数
+    except:
+        FrameNum = 1  # 未定义帧总数则仅播放一帧
+    try:
+        Fps = int(fname[2][:2])  # 读取文件名中fps
+    except:
+        Fps = 30  # 未定义fps则默认为30
+
+    # 如果有音乐，则初始化相关内容
+    if HasMusic:
+        Music = iter(MUSIC)  # 创建乐谱迭代器
+        beep = machine.PWM(Pin(BEEP_PIN))  # 定义蜂鸣器对象
+        time0 = Timer(0)  # 创建time0定时器对象：控制蜂鸣器发声
+        time0.init(period=100, mode=Timer.ONE_SHOT, callback=time0_irq)
+
+    # 视频定时器初始化
     time1 = Timer(1)  # 创建time1定时器对象：控制oled逐帧显示图像，形成视频
-    time1.init(freq=fps, mode=Timer.PERIODIC, callback=time1_irq)
+    time1.init(freq=Fps, mode=Timer.PERIODIC, callback=time1_irq)
