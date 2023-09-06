6a78a619cfc5a069d0d4940171adc054b1c0ab23
456258hf <456258hf@gmail.com>
2023-09-06 23:23:28 +0800

    added resize option

--- frames2videohex.py
+++ frames2videohex.py
@@ -5,6 +5,36 @@ from config import *
 from PIL import Image
 
 
+def ImageResizeNoStretch(image: Image) -> Image:
+    # 获取图像尺寸
+    image_width, image_height = image.size
+
+    # 计算比例
+    width_ratio = SCREEN_WIDTH / image_width
+    height_ratio = SCREEN_HEIGHT / image_height
+
+    # 计算适应后的尺寸
+    if width_ratio < height_ratio:
+        new_width = SCREEN_WIDTH
+        new_height = int(image_height * width_ratio)
+    else:
+        new_width = int(image_width * height_ratio)
+        new_height = SCREEN_HEIGHT
+
+    # 创建一个空白画布
+    canvas = Image.new("RGB", (SCREEN_WIDTH, SCREEN_HEIGHT), PADDING_COLOR)
+
+    # 计算图像居中位置
+    x_offset = (SCREEN_WIDTH - new_width) // 2
+    y_offset = (SCREEN_HEIGHT - new_height) // 2
+
+    # 将图像粘贴到画布上
+    canvas.paste(image.resize((new_width, new_height),
+                 Image.Resampling.LANCZOS), (x_offset, y_offset))
+
+    return canvas
+
+
 def Binarization2String(image: Image) -> str:  # 将图像二值化为字符串
     pixels = image.getdata()
     image_str = ""
@@ -12,16 +42,6 @@ def Binarization2String(image: Image) -> str:  # 将图像二值化为字符串
         light_or_dark = 1 if pixel > 127 else 0
         image_str += str(light_or_dark ^ REVERSED_COLOR)
     return image_str
-
-
-def Padding(image_str: str) -> str:  # 对图像小于12864的部分补0
-    output = ""
-    for i in range(RESIZE_HEIGHT):
-        row = image_str[i*RESIZE_WIDTH:(i+1)*RESIZE_WIDTH]
-        row += "0" * (128-RESIZE_WIDTH)
-        output += row
-    output += "0" * ((64-RESIZE_HEIGHT)*128)
-    return output
 
 
 def Transform(input_str: str) -> str:  # 对图像进行处理，使得可以直接写进ssd1306显存
@@ -56,17 +76,19 @@ if __name__ == '__main__':
             # 打开图像：
             frame = PIL.Image.open(os.path.join(ORIGINAL_FRAME_FOLDER, frame))
             # 调整图像大小：
-            frame_r = frame.resize((RESIZE_WIDTH, RESIZE_HEIGHT))
+            if FRAME_STRETCH:
+                frame_r = frame.resize(
+                    (SCREEN_WIDTH, SCREEN_HEIGHT), Image.Resampling.LANCZOS)
+            else:
+                frame_r = ImageResizeNoStretch(frame)
             # 灰度化图像：
             frame_rc = frame_r.convert("L")
             # 将图像二值化为字符串：
             frame_str = Binarization2String(frame_rc)
-            # 对图像小于12864的部分补0：
-            frame_strp = Padding(frame_str)
             # 对图像进行处理，使得可以直接写进ssd1306缓存：
-            frame_strpt = Transform(frame_strp)
+            frame_strt = Transform(frame_str)
             # 将字符串转换为bytes类型的对象：
-            frame_bytes = int(frame_strpt, 2).to_bytes(1024, byteorder='big')
+            frame_bytes = int(frame_strt, 2).to_bytes(1024, byteorder='big')
             # 在文件最后写入当前帧数据：
             with open(hexname, 'ab') as f:
                 f.write(frame_bytes)
@@ -75,6 +97,9 @@ if __name__ == '__main__':
             bytes = f.read()
             hash = hashlib.sha256(bytes).hexdigest()
         newhexname = f'video-{count:05d}p-{OUTPUT_FPS:02d}fps-{hash[:8]}.hex'
-        os.rename(hexname, newhexname)
-        
-        print(f'Converted! Check the hexfile \{newhexname}')
+        try:
+            os.rename(hexname, newhexname)
+            print(f'Converted! Check the hexfile {newhexname}')
+        except FileExistsError:
+            print(
+                f"Hexfile {newhexname} with same content has already been generated")
